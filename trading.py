3403aac723d0f67fd8ad4c67e50d49e0e36ffd1c
HADHRI <hadhri.anas@gmail.com>
2020-01-17 17:37:31 +0100

    get order book of an asset

--- trading.py
+++ trading.py
@@ -1,5 +1,6 @@
-import requests
-import json
+import json,hmac, hashlib, time, requests, base64
+from requests.auth import AuthBase
+from conf import conf
 #get list of all availables cryptocurrencies and display it 
 def get_all_crypto_currency():
     url = 'https://rest.coinapi.io/v1/assets'
@@ -20,11 +21,49 @@ def get_depth(direction, pair):
     else:
         #I print all if there's a problem with the direction input 
         print(response.json())
-        
+
+# Create custom authentication for Exchange
+class CoinbaseExchangeAuth(AuthBase):
+    def __init__(self, api_key, secret_key, passphrase):
+        self.api_key = api_key
+        self.secret_key = secret_key
+        self.passphrase = passphrase
+
+    def __call__(self, request):
+        timestamp = str(time.time())
+        message = timestamp + request.method + request.path_url + (request.body or b'').decode()
+        hmac_key = base64.b64decode(self.secret_key)
+        signature = hmac.new(hmac_key, message.encode(), hashlib.sha256)
+        signature_b64 = base64.b64encode(signature.digest()).decode()
+
+        request.headers.update({
+            'CB-ACCESS-SIGN': signature_b64,
+            'CB-ACCESS-TIMESTAMP': timestamp,
+            'CB-ACCESS-KEY': self.api_key,
+            'CB-ACCESS-PASSPHRASE': self.passphrase,
+            'Content-Type': 'application/json'
+        })
+        return request
+
+#We will use this in order to request private API 
+auth = CoinbaseExchangeAuth(conf['API_KEY'], conf['API_SECRET_KEY'], conf['API_PASS'])
+
+#This API is private , we will use the key in order to request it , otherwise we will be forbidden 403 returned code
+def get_book_order_of_asset(asset):
+    response = requests.get('https://api-public.sandbox.pro.coinbase.com/products/{}/book'.format(asset), auth=auth) 
+    if(response.status_code == 404):
+        print("not found , please verify the spelling of the asset ")
+        return
+    print(response.json())
+     
+
+
+
     
     
-#Test functions 
+#Test functions  
 
 #get_all_crypto_currency()
-get_depth('bid','BTC-USD')
+#get_depth('bid','BTC-USD')
+get_book_order_of_asset('BTC-USD')
 
