051f0d1934d604a40b39ddf6c7c89b86f2f1af41
HADHRI <hadhri.anas@gmail.com>
2020-01-17 15:51:29 +0100

    Adding depth method

--- trading.py
+++ trading.py
@@ -6,4 +6,25 @@ def get_all_crypto_currency():
     response = requests.get(url)
     print(json.dumps(response.json()))
 
-get_all_crypto_currency()
+
+def get_depth(direction, pair):
+    url_coinBase_pro='https://api.pro.coinbase.com/products/{}/ticker'.format(pair)
+    response = requests.get(url_coinBase_pro)
+    if(response.status_code == 404):
+        print("not found , please verify the spelling of the asset . for example BTC-USD and not BTCUSD")
+        return
+    if(direction =='ask'):
+        print(response.json()['ask'])
+    elif direction == 'bid':
+        print(response.json()['bid'])
+    else:
+        #I print all if there's a problem with the direction input 
+        print(response.json())
+        
+    
+    
+#Test functions 
+
+#get_all_crypto_currency()
+get_depth('bid','BTC-USD')
+
